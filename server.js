fbc9eff66f9295395a3f187812d05e5483850281
hewseo <hewseo03@gmail.com>
2024-02-19 01:59:42 +0900

    서버 포트 변경

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const PORT = 3000;
 
 app = express()
 server = http.createServer(app);
@@ -12,6 +13,6 @@ app.get('/login', function (req, res) {
   res.sendFile('login.html');
 });
 
-server.listen(8000, function () {
-  console.log('Express server listening on port ' + server.address().port);
+server.listen(PORT, function () {
+  console.log('Express server listening on port ' + PORT);
 });
