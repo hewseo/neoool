6e2501bea814d1a3f46ee60d921a24a9c4377a63
hewseo <hewseo03@gmail.com>
2024-02-19 02:16:16 +0900

    수정

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ app = express()
 server = http.createServer(app);
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+ // res.sendFile('index.html');
+    res.send("기본페이지");
 });
 
 app.get('/login', function (req, res) {
-  res.sendFile('login.html');
+ // res.sendFile('login.html');
+    res.send("로그인페이지");
 });
 
 server.listen(PORT, function () {
