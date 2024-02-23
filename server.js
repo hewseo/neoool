462bc8dc7f225caa62b21072e9e4100ba7522b49
hewseo <hewseo03@gmail.com>
2024-02-23 15:02:38 +0900

    테스트

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express()
 const server = http.createServer(app);
 
 app.get('/', function(req, res) {
- // res.sendFile('index.html');
-    res.send("기본페이지");
+  res.sendFile('index.html');
+  console.log("기본페이지");
 });
 
 app.get('/login', function (req, res) {
- // res.sendFile('login.html');
-    res.send("로그인페이지");
+  res.sendFile('login.html');
+  console.log("로그인페이지");
 });
 
 server.listen(PORT, function () {
